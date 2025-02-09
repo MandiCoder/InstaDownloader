ba994173c9cca9f07f93e8a153b69e7e9ab80983
mandicoder <mandicoder@proton.me>
2025-02-09 11:30:04 +0000

    bug fix 2

--- Notion.py
+++ Notion.py
@@ -15,13 +15,13 @@ class Logs():
         }
         
     def fetch_data(self):
-        response = requests.post(f'https://api.notion.com/v1/databases/{os.getenv("DATABASE_LOGS")}/query', headers=self._header)
+        response = requests.post(f'https://api.notion.com/v1/databases/{os.getenv('DATABASE_LOGS')}/query', headers=self._header)
         return response
         
         
     def send_data(self, username:str, url:str):
         payload = {
-            "parent": {"database_id": os.getenv("DATABASE_LOGS")},
+            "parent": {"database_id": os.getenv('DATABASE_LOGS')},
             "properties": {
                 "Usuario": {
                     "title": [{"text": {"content": username}}]
@@ -45,19 +45,19 @@ class Usuario():
         dotenv.load_dotenv()
         
         self._header = {
-            "Authorization": f"Bearer {os.getenv("NOTION_TOKEN")}",
+            "Authorization": f"Bearer {os.getenv('NOTION_TOKEN')}",
             "Content-Type": "application/json",
             "Notion-Version": "2022-06-28"
         }
         
     def fetch_data(self):
-        response = requests.post(f'https://api.notion.com/v1/databases/{os.getenv("DATABASE_ID")}/query', headers=self._header)
+        response = requests.post(f'https://api.notion.com/v1/databases/{os.getenv('DATABASE_ID')}/query', headers=self._header)
         return response
         
         
     def send_data(self, id:str, username:str, first_name:str):
         payload = {
-            "parent": {"database_id": os.getenv("DATABASE_ID")},
+            "parent": {"database_id": os.getenv('DATABASE_ID')},
             "properties": {
                 "Nombre": {
                     "title": [{"text": {"content": first_name}}]
