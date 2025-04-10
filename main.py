231ce204f2e256856ca05f9f95233c0eb471b0a3
mandicoder <mandicoder@proton.me>
2025-04-10 16:37:58 -0400

    descarga de facebook

--- main.py
+++ main.py
@@ -2,32 +2,57 @@ from email import message
 from bot_init import Bot
 from pyrogram import filters
 from pyrogram.types import Message
-from os import listdir
+from os import listdir, unlink
 from os.path import join
 from shutil import rmtree
 from Notion import Logs, Usuario
-from datetime import datetime
 import instaloader
+from yt_dlp import YoutubeDL
+import subprocess
 
 
 bot = Bot()
 app = bot.app
-
-def descargar_reel(url, msg: Message = None):
+# https://www.facebook.com/share/r/1EFMRMQGDS/
+# https://www.instagram.com/luciareinarodriguezpari/reel/C-c_PRJJ6Tl/?igsh=amQzM21xbGVhZDBs
+def descargar_video(url, msg: Message = None):
+    sms = msg.reply("**⬇️ DESCARGANDO VIDEO...**")
     try:
-        sms = msg.reply("**⬇️ DESCARGANDO VIDEO...**")
-        L = instaloader.Instaloader()
-        reel_code = url.split("/reel/")[1].split("/")[0]
-        post = instaloader.Post.from_shortcode(L.context, reel_code)
-        id = post.mediaid
-        caption = post.caption
-        L.download_post(post, target=id)
-        video = join(str(id), [vid for vid in listdir(str(id)) if vid.endswith('mp4')][0])
-        thumb = join(str(id), [vid for vid in listdir(str(id)) if vid.endswith('jpg')][0])
+        if "www.facebook.com" in url:
+            ydl_opts = {
+                'format': 'bestvideo+bestaudio/best',
+                'outtmpl': '%(id)s.%(ext)s',
+                'quiet': True,
+            }
+            with YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=True)
+                video = ydl.prepare_filename(info_dict)
+                caption = info_dict.get('title', 'Video descargado de Facebook')
+
+            thumb = f"{video}.jpg"
+            subprocess.run(
+                ["ffmpeg", "-i", video, "-ss", "00:00:01.000", "-vframes", "1", thumb],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+        
+        elif "www.instagram.com" in url:
+            L = instaloader.Instaloader()
+            reel_code = url.split("/reel/")[1].split("/")[0]
+            post = instaloader.Post.from_shortcode(L.context, reel_code)
+            id = post.mediaid
+            caption = post.caption
+            L.download_post(post, target=id)
+            video = join(str(id), [vid for vid in listdir(str(id)) if vid.endswith('mp4')][0])
+            thumb = join(str(id), [vid for vid in listdir(str(id)) if vid.endswith('jpg')][0])
+
         sms.edit_text("**⬆️ SUBIENDO VIDEO...**")
         msg.reply_video(video=video, caption=caption, thumb=thumb, quote=True)
         sms.delete()
         rmtree(str(id))
+        unlink(video)
+        unlink(thumb)
+
     except Exception as e:
         msg.reply(f"ERROR: {e}")
 
@@ -39,13 +64,12 @@ def exist_user(id:int):
     else:
         return False
     
-    
 
 @app.on_message(filters.command("start"))
 def saludar(app, msg:Message):
     text = f"""**
-¡Hola! {msg.from_user.first_name} 🌟 Bienvenido a InstaTG. Aquí puedes descargar reels de Instagram de manera rápida y sencilla. 🎥✨
-Solo envía el enlace del reel que deseas descargar y listo 🚀. ¡Que disfrutes tus videos favoritos! 🎉
+¡Hola! {msg.from_user.first_name} 🌟 Bienvenido a InstaTG. Aquí puedes descargar reels de Instagram y Facebook de manera rápida y sencilla. 🎥✨
+Solo envía el enlace del video que deseas descargar y listo 🚀. ¡Que disfrutes tus videos favoritos! 🎉
 Si necesitas ayuda, no dudes en preguntar al creador __@MandiCoder__.**"""
     msg.reply(text)
     
@@ -66,8 +90,7 @@ def download_video(app, msg:Message):
         )
     url = msg.text
     Logs().send_data(username=msg.from_user.username, url=url)
-    descargar_reel(url, msg)
+    descargar_video(url, msg)
 
 
-# descargar_reel("https://www.instagram.com/reel/DEY0zEnNWnU/?igsh=MXVxOW5renBpb256eg==")
 bot.run()
